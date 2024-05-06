b7840456eeaa0d3a4000a3d5e7fe145b82351bca
hillan tazidi <160846687+hillan17@users.noreply.github.com>
2024-05-06 20:09:29 +0700

    Add files via upload

---
+++ Java Collection/AntrianRS.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class AntrianRS {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    Queue<String> queue = new LinkedList<String>();
+
+    System.out.println("==================== SELAMAT DATANG DI RSUB ====================");
+    System.out.print("Masukkan nama pasien (Ketikkan 'Stop' untuk berhenti): ");
+    String pasien = input.nextLine();
+
+    while (!pasien.equalsIgnoreCase("stop")) {
+      queue.add(pasien);
+      System.out.print("Masukkan nama pasien (Ketikkan 'Stop' untuk berhenti): ");
+      pasien = input.nextLine();
+    }
+
+    System.out.println("\n========== DAFTAR PASIEN ==========");
+    int queueNum = 1;
+    for(String pasienList : queue) {
+      System.out.println(queueNum + ". " + pasienList);
+      queueNum++;
+    }
+
+    System.out.println("\n========== RAWAT PASIEN ==========");
+    int serveNum = 1;
+    while (!queue.isEmpty()) {
+      String servePasien = queue.poll();
+      System.out.println(serveNum + ". " + servePasien + " sedang dirawat.");
+      serveNum++;
+    }
+  
+      if (queue.isEmpty()) {
+        System.out.println("\n----- PASIEN DALAM ANTRIAN SUDAH DIRAWAT -----");
+      }
+      input.close();
+    }
+  }
