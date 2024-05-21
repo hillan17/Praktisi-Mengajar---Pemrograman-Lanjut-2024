f113f731a690c051c9a7ba734dbe74eef83a05e3
hillan tazidi <160846687+hillan17@users.noreply.github.com>
2024-05-21 23:52:51 +0700

    Main.java

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,10 +1,6 @@
-import javax.swing.SwingUtilities;
-
 public class Main {
    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
             AntrianRS frame = new AntrianRS();
             frame.setVisible(true);
-        });
     }
 }
